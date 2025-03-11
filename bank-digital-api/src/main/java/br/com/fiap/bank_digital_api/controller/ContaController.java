291086c7279a4268b28852ac23de0e7452c83f62
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-03-11 18:41:52 -0300

    feat get por Id, get conta

--- bank-digital-api/src/main/java/br/com/fiap/bank_digital_api/controller/ContaController.java
+++ bank-digital-api/src/main/java/br/com/fiap/bank_digital_api/controller/ContaController.java
@@ -3,11 +3,10 @@ package br.com.fiap.bank_digital_api.controller;
 import br.com.fiap.bank_digital_api.model.Conta;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,5 +26,22 @@ public class ContaController {
         repository.add(conta);
         return ResponseEntity.status(201).body(conta);
     }
+    @GetMapping("/contas/{id}")
+    public ResponseEntity<Conta> get(@PathVariable Long id){
+        log.info("Buscando conta pelo id " + id);
+        return ResponseEntity.ok(getConta(id));
+    }
+
+    private Conta getConta(Long id) {
+        return repository.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst()
+                .orElseThrow(
+                    () -> new ResponseStatusException(
+                            HttpStatus.NOT_FOUND,
+                            "Conta não encontrada")
+                );
+    }
+
 
 }
