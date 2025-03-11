7fce2533792373d763238a8bff261b2d2b651a1c
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-03-11 14:14:09 -0300

    projeto bank digital api

---
+++ bank-digital-api/bank-digital-api/src/main/java/br/com/fiap/bank_digital_api/BankDigitalApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bank_digital_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDigitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankDigitalApiApplication.class, args);
+	}
+
+}
---
+++ bank-digital-api/bank-digital-api/src/test/java/br/com/fiap/bank_digital_api/BankDigitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bank_digital_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDigitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
